d77c397632215af8372e7676293aaf24fdade3df
Nick Ellis <217nickellis@gmail.com>
2018-10-02 19:44:18 -0400

    found out how to get four downward neighbors

--- max_pth_sum.py
+++ max_pth_sum.py
@@ -30,31 +30,54 @@ def str_num_conv(list_of_lists):
 
     return list_of_lists
 
+def leftNeighbors(lyst, i, j):
+    life = [lyst[i][j], lyst[i+1][j], lyst[i+2][j], lyst[i+3][j]]
+    return life
+
+def rightNeihgbors(lyst, i,  j):
+    life = [lyst[i][j], lyst[i+1][j+1], lyst[i+2][j+2], lyst[i+3][j+3]]
+    return life
 
 def getNeighbors(lyst, i, j):
     life = [lyst[i+1][j], lyst[i+1][j+1]]
     return life
 
 def genNeighbors(list_of_list):
-    dict = {}
+    left = []
+    right = []
+
     index_i = 0
     num = 0
     for i in list_of_list:
         index_j = 0
         for j in i:
-            if index_i < 14:
-                jay = str(j)+'-'+str(num)
-                dict.update({jay: getNeighbors(list_of_list, index_i, index_j)})
+            if index_i < 12:
+                left.append(leftNeighbors(list_of_list, index_i, index_j))
+                right.append(rightNeihgbors(list_of_list, index_i, index_j))
             else:
                 pass
             num += 1
             index_j += 1
         index_i += 1
-    return  dict
+    print(right)
+    print(left)
+
+def left_paths(dictionary):
+    #i want to append the 4 downward neighbors to the list
+    lyst = []
+    addy = None
+    for i in range(0, 4):
+        #append addy
+        return None
+        #set addy to the left neigh of addy
 
 
+def right_paths(dictionary):
+    return None
+
+i=0
+j=0
 lol = liststr_listlist(tri_str)
-print(len(lol))
 tri_list = str_num_conv(lol)
 map = genNeighbors(tri_list)
 
